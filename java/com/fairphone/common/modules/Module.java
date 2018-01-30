fe36faf8e1a0b54029f55a7303ac5cea231be3dc
Borjan Tchakaloff <borjan@fairphone.com>
2018-01-30 15:33:34 +0100

    Define the Module base class as a foundation for the actual modules
    Change-Id: I6a4dbbfdb0d128d2093141dbff04be579b2c9ef6

---
+++ java/com/fairphone/common/modules/Module.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Fairphone B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fairphone.common.modules;
+
+import android.annotation.NonNull;
+
+/**
+ * A Fairphone 2 module.
+ * <p>
+ * A plain old Java object that holds information about a module.
+ */
+public abstract class Module {
+
+    /** The installed, non-translatable module version identifier. */
+    @NonNull
+    private final String mVersionId;
+
+    /* package */ Module(@NonNull String versionId) {
+        mVersionId = versionId;
+    }
+
+    /**
+     * @return The installed, non-translatable module version identifier or
+     * <code>null</code>.
+     */
+    @NonNull
+    public String getVersionId() {
+        return mVersionId;
+    }
+
+}
